809884328b96d2a2ca75e88afb78a5824072bb8e
Mikhail Mikheev <mmvsha73@gmail.com>
2021-04-29 17:14:38 +0400

    add defi saver

--- config/appsList.js
+++ config/appsList.js
@@ -49,6 +49,11 @@ const safeAppsConfig = [
     url: `${process.env.REACT_APP_IPFS_GATEWAY}/QmX31xCdhFDmJzoVG33Y6kJtJ5Ujw8r5EJJBrsp8Fbjm7k`,
     networks: [ETHEREUM_NETWORK.MAINNET, ETHEREUM_NETWORK.RINKEBY],
   },
+  // DeFi Saver
+  {
+    url: `https://app.defisaver.com/`,
+    networks: [ETHEREUM_NETWORK.MAINNET],
+  },
   // dHedge
   {
     url: `https://app.dhedge.org/`,
